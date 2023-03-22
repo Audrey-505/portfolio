09002689e6dd203f5530ee38fbe1131731621d3e
Audrey Alcantara <adrianaquin22@gmail.com>
2023-03-22 15:59:15 -0400

    added message and name validation on contact form and adjusted footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 export default function Footer(){
     return (
-      <div className="card">
+      <div className="card text-center">
         {/* <ul className="list-group list-group-flush"> */}
         <ul className="list-group list-group-horizontal-lg">
-                <li className="list-group-item"><a href="https://www.google.com/">Extra</a></li>
-                <li className="list-group-item"><a href="https://github.com/Audrey-505">GitHub</a></li>
-                <li className="list-group-item"><a href="https://www.linkedin.com/in/adriana-alcantara-96b9b4160/">LinkedIn</a></li>
+                <li className="list-group-item"><a href="mailto:adrianaquin22@gmail.com"><img src={require("./images/email.png")} width="20%"/></a></li>
+                <li className="list-group-item"><a href="https://github.com/Audrey-505"><img src={require("./images/github.png")} width="10%"/></a></li>
+                <li className="list-group-item"><a href="https://www.linkedin.com/in/adriana-alcantara-96b9b4160/"><img src={require("./images/linkedin.png")} width="10%"/></a></li>
             </ul>
 
             <div className="text-center">
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -7,8 +7,33 @@ export default function Contact(){
     let [email, setEmail] = useState('')
     let [error, setError] = useState(null)
 
+    let [message, setMessage] = useState('')
+    let [warning, setWarning] = useState(null)
+
+    let [name, setName] = useState('')
+
+
 
     const handleInput = function (event){
+        setMessage((event.target.value))
+        console.log(event.target.value)
+    }
+
+    const handleName = function (event){
+        setName((event.target.value))
+    }
+
+
+    useEffect(function(){
+        if(message.length === 0 || name.length === 0){
+            setWarning('Please enter a message and a name')
+        } else {
+            setWarning(null)
+        }
+    }, [message])
+
+
+    const handleEmail = function (event){
         // var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
         setEmail((event.target.value))
         // return re.test(email)
@@ -33,10 +58,10 @@ export default function Contact(){
             <h3>Please complete the form below to reach me with any questions or comments</h3>
             <div>
                 <form style={formStyle} className="form">
-                    <p>{error}</p>
-                    <input name="fName" style={inputStyle}  type="text" placeholder="First Name"/>
-                    <input name ="email" style={inputStyle} onChange={handleInput}type="email" placeholder="Email"/>
-                    <input name="message" style={inputStyle} type="text" placeholder="Message"/>
+                    <p>{error}</p><p>{warning}</p>
+                    <input name="fName" style={inputStyle} onChange={handleName} type="text" placeholder="First Name"/>
+                    <input name ="email" style={inputStyle} onChange={handleEmail} type="email" placeholder="Email"/>
+                    <input name="message" style={inputStyle} onChange={handleInput} type="text" placeholder="Message"/>
                     <button className="btn btn-primary" type="button">Submit</button>
                 </form>
             </div>
